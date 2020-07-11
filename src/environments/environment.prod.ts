dc788acebb50ad6d6f9a5d02128f3ea16f4653c8
sima paztarian <baziyar.sima@gmail.com>
2020-07-11 11:34:25 +0430

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBR4cEXrRdZlwtDn6XqiqXFO8Jvi5aSOvw",
+    authDomain: "oshop-ddf37.firebaseapp.com",
+    databaseURL: "https://oshop-ddf37.firebaseio.com",
+    projectId: "oshop-ddf37",
+    storageBucket: "oshop-ddf37.appspot.com",
+    messagingSenderId: "772704173819",
+    appId: "1:772704173819:web:f71d785a7e3d5c4322fa5e",
+    measurementId: "G-Z1KPQYGE7D"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBR4cEXrRdZlwtDn6XqiqXFO8Jvi5aSOvw",
+    authDomain: "oshop-ddf37.firebaseapp.com",
+    databaseURL: "https://oshop-ddf37.firebaseio.com",
+    projectId: "oshop-ddf37",
+    storageBucket: "oshop-ddf37.appspot.com",
+    messagingSenderId: "772704173819",
+    appId: "1:772704173819:web:f71d785a7e3d5c4322fa5e",
+    measurementId: "G-Z1KPQYGE7D"
+  }
 };
 
 /*
